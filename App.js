3678428d052dd6d40eddc40ffa755b5a3539f90a
gauraklein <gaurakklein@gmail.com>
2019-11-17 14:41:15 -0600

    able to get a basic weather view showing up but working on fixing display issues

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Weather from './components/Weather.js';
+
+export default class App extends React.Component {
+  state = {
+    isLoading: false
+  };
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoading ? <Text>Fetching the Weather</Text> : (
+          <View>
+            <Weather />
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
